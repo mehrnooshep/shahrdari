5a8fd62412b1937024c7fc57a5fe18b84557214c
mehrnooshep <mehrnoosh.ebrahimipour@gmail.com>
2013-08-05 06:42:31 -0700

    models added

--- myApp/models.py
+++ myApp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class RollCall(models.Model):
+	date = models.DateField(auto_now_add=True, verbose_name=u"تاریخ")
+	entrance_time = models.DateField(auto_now_add=True, verbose_name=u"زمان ورود")
+	exit_time = models.DateField(auto_now_add=True, verbose_name=u"زمان خروج")
