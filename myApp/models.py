ce9cd13b3cbd08d9615affae53e9dc7a9cbd6aab
mehrnooshep <mehrnoosh.ebrahimipour@gmail.com>
2013-08-18 03:10:07 -0700

    :p

--- myApp/models.py
+++ myApp/models.py
@@ -1,12 +1,44 @@
-<<<<<<< HEAD
 # -*- encoding: utf-8 -*-
-=======
-#coding=UTF-8
->>>>>>> c0226b0e6f714e8f17a1eb50b83e080f7901b4b6
-from django.db import models
-
+from django.db import models, connection
 # Create your models here.
-class RollCall(models.Model):
-	date = models.DateField(auto_now_add=True, verbose_name=u"تاریخ")
-	entrance_time = models.DateField(auto_now_add=True, verbose_name=u"زمان ورود")
-	exit_time = models.DateField(auto_now_add=True, verbose_name=u"زمان خروج")
+
+class ETL(models.Model):
+	subsystem = models.CharField(max_length=40 )
+	subsystemName = models.CharField(max_length=40 )
+	ETLStart = models.DateTimeField( null=True)
+	ETLEnd = models.DateTimeField( null=True)
+	ETLSuccess = models.BooleanField(default=False)
+	SSASStart = models.DateTimeField( null=True)
+	SSASEnd = models.DateTimeField( null=True)
+	SSASSuccess = models.BooleanField(default=False)
+	# filterId= models.IntegerField()
+
+	# def save(self, *args, **kwargs):
+	# 	self.filterId = self.set_filter_id()
+	# 	return super(ETL, self).save(*args, **kwargs)
+
+	# def set_filter_id(self):
+	# 	if self.subsystemName == '137':
+	# 		 self.filterId=1
+	# 	elif self.subsystemName == '1888':
+	# 		self.filterId=2
+	# 	elif self.subsystemName == 'آموزش':
+	# 		self.filterId=3
+	# 	elif self.subsystemName == 'ارزشیابی کارکنان':
+	# 		self.filterId=4
+	# 	elif self.subsystemName == 'ارزشیابی مدیران':
+	# 		self.filterId=5
+	# 	elif self.subsystemName == 'ارزشیابی عملکرد مناطق':
+	# 		self.filterId=6
+	# 	elif self.subsystemName == 'عوارض خودرو':
+	# 		self.filterId=7
+	# 	elif self.subsystemName == 'فنی-عمرانی':
+	# 		self.filterId=8
+
+	def gregorian_to_persian(self, date):
+		cursor = connection.cursor
+		try:
+			cursor.callproc("[dbo].[GregorianToPersian]" ,[str(date)])
+			return cursor.return_value
+		finally:
+			cursor.close()
--- myApp/views.py
+++ myApp/views.py
@@ -1,4 +1,6 @@
+#-*- encoding:utf-8 -*-
 # Create your views here.
+import mimetypes
 from django.shortcuts import render
 from django.http import HttpResponse
 from myApp.models import *
@@ -12,13 +14,13 @@ def index(request):
 	print "hellooooo"
 	return render(request, 'mybase.html', {})
 def show_table(request):
-	#request.get['username']
 	#request.post
 	#l = SaleBill.objects.all()[0]
 	#return HttpResponse('{0} and {1} and {2}'.format(l.saleDate, l.totalPrice, l.costumer.balance))
 	return render(request, 'show_table.html', {})
 
 def table_data(request, filter_id):
+
 	filter_id = int(filter_id)
 	try:
 		page = int(request.GET['page'])
@@ -28,15 +30,43 @@ def table_data(request, filter_id):
 		rowcount = int(request.GET['rows'])
 	except:
 		rowcount = 20
+	print filter_id
+	data =[]
+	if filter_id ==1 :
+		print "here"
+		data = ETL.objects.filter(subsystemName = '137')[(page-1)*rowcount: (page)*rowcount];
+		# if etls.subsystemName == '137':
+		# 	etls.filterId=1
+		# elif self.subsystemName == '1888':
+		# 	self.filterId=2
+		# elif self.subsystemName == 'آموزش':
+		# 	self.filterId=3
+		# elif self.subsystemName == 'ارزشیابی کارکنان':
+		# 	self.filterId=4
+		# elif self.subsystemName == 'ارزشیابی مدیران':
+		# 	self.filterId=5
+		# elif self.subsystemName == 'ارزشیابی عملکرد مناطق':
+		# 	self.filterId=6
+		# elif self.subsystemName == 'عوارض خودرو':
+		# 	self.filterId=7
 
-	data = RollCall.objects.all()[(page-1)*rowcount: (page)*rowcount];
+	# print(data.count())
 	jdata = {"total":len(data), "page":page, "records":rowcount, "rows":[]}
-	
+	print "beforee"
+	# print(data.get(id=2).id)
 	for rc in data:
+		print ("forrrr")
 		row = {}
-		row['date'] = unicode(rc.date)
-		row['entrance'] = unicode(rc.entrance_time)
-		row['exit'] = unicode(rc.exit_time)
+		row['name'] = unicode(rc.subsystemName)
+		row['ETLStart'] = unicode(rc.ETLStart)
+		row['ETLEnd'] = unicode(rc.ETLEnd)
+		row['ETLSuccess'] = unicode(rc.ETLSuccess)
+		row['SSASStart'] = unicode(rc.SSASStart)
+		row['SSASEnd'] = unicode(rc.SSASEnd)
+		row['SSASSuccess'] = unicode(rc.SSASSuccess)
 		jdata['rows'].append(row)
+		print rc.id
+
 	return HttpResponse(json.dumps(jdata), content_type="application/json")
+
 
